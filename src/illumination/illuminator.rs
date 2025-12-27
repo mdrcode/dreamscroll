b2a026300e44761694c084f1efe7073ea820317b
Mike Repass <mike.repass@gmail.com>
2025-12-27 13:09:39 -0500

    Refactor Illuminator to use Arc for OneShotQueue and improve worker spawning

--- src/illumination/illuminator.rs
+++ src/illumination/illuminator.rs
@@ -6,27 +6,29 @@ use sea_orm::EntityTrait;
 use crate::model::capture;
 use crate::{common, database};
 
+pub fn make(db: Arc<database::DbHandle>) -> Box<dyn Illuminator> {
+    Box::new(SimpleIlluminator {
+        db,
+        queue: Arc::new(common::OneShotQueue::new()),
+    })
+}
+
 #[async_trait]
 pub trait Illuminator: Send + Sync {
     async fn run(&self) -> anyhow::Result<()>;
 }
 
-pub fn make(db: Arc<database::DbHandle>) -> Box<dyn Illuminator> {
-    Box::new(SimpleIlluminator {
-        db,
-        queue: common::OneShotQueue::new(),
-    })
-}
-
 pub struct SimpleIlluminator {
     db: Arc<database::DbHandle>,
-    queue: common::OneShotQueue<i32>,
+    queue: Arc<common::OneShotQueue<i32>>,
 }
 
 #[async_trait]
 impl Illuminator for SimpleIlluminator {
     async fn run(&self) -> anyhow::Result<()> {
-        println!("Illuminator starting...");
+        (0..2).for_each(|_| {
+            tokio::spawn(illuminate_worker(self.db.clone(), self.queue.clone()));
+        });
 
         loop {
             // Fetch all captures from the database
@@ -43,6 +45,18 @@ impl Illuminator for SimpleIlluminator {
             println!("Found {} captures in the database.", captures.len());
 
             tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        }
+    }
+}
+
+async fn illuminate_worker(_db: Arc<database::DbHandle>, queue: Arc<common::OneShotQueue<i32>>) {
+    loop {
+        if let Some(capture_id) = queue.pop_next() {
+            tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+            queue.complete(capture_id);
+            println!("Worker thread illuminated capture ID {}", capture_id);
+        } else {
+            tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
         }
     }
 }
