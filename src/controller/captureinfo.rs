48585036874df44b7d0e97b7f4003748141aadef
Mike Repass <mike.repass@gmail.com>
2026-01-04 16:59:13 -0500

    Move the (primitive) search logic into controller

--- src/controller/captureinfo.rs
+++ src/controller/captureinfo.rs
@@ -119,4 +119,37 @@ impl CaptureInfo {
 
         Ok(capture_ids)
     }
+
+    pub async fn search_by_illuminations(
+        db: &DbHandle,
+        query: &str,
+    ) -> anyhow::Result<Vec<CaptureInfo>, AppError> {
+        if query.is_empty() {
+            return Ok(vec![]);
+        }
+
+        // Find illuminations that contain the search query
+        let capture_ids_with_match = illumination::Entity::find()
+            .filter(illumination::Column::Content.contains(query))
+            .column(illumination::Column::CaptureId)
+            .distinct()
+            .all(&db.conn)
+            .await?
+            .into_iter()
+            .map(|i| i.capture_id)
+            .collect::<Vec<i32>>();
+
+        // Get unique capture IDs
+
+        let captures = capture::Entity::load()
+            .filter(capture::Column::Id.is_in(capture_ids_with_match))
+            .with(media::Entity)
+            .with(illumination::Entity)
+            .all(&db.conn)
+            .await?;
+
+        let capture_info = captures.into_iter().map(|c| CaptureInfo::new(c)).collect();
+
+        Ok(capture_info)
+    }
 }
