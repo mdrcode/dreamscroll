1a7425df9a68bf7d9d735086281c5fb02db772fe
Mike Repass <mike.repass@gmail.com>
2026-02-02 20:37:11 -0500

    polishing the fake-gcs emulator workflow some

--- docker-fake-gcs-emulator.sh
+++ docker-fake-gcs-emulator.sh
@@ -1,10 +1,15 @@
 docker run -d \
 --name fake-gcs \
 -p 4443:4443 \
--v $PWD/localdev/gcloud_storage/:/gcloud-storage \
+-v $PWD/localdev/gcloud_storage/:/storage \
 fsouza/fake-gcs-server \
 -scheme http \
--data /gcloud-storage
+-data /storage
+
+curl -X POST -H "Content-Type: application/json" \
+http://localhost:4443/storage/v1/b \
+-d '{"name": "dreamscroll-test1"}'
+
 
 # NOTE, if you get "Not Found" errors when attempting to upload to emulator
 # You probably need to create the bucket manually, since the emulator
