64eb86383eebc27f25b7ee3b84dd8d7c2f870d14
Mike Repass <mike.repass@gmail.com>
2026-02-15 07:07:43 -0500

    fix minor bug with import_capture

--- src/api/import/import_capture.rs
+++ src/api/import/import_capture.rs
@@ -6,10 +6,10 @@ use crate::{api, database::DbHandle, model, storage};
 pub async fn import_capture(
     db: &DbHandle,
     user_id: i32,
-    media1: storage::StorageHandle,
+    media_handle: storage::StorageHandle,
     created_at: chrono::DateTime<Utc>,
 ) -> Result<model::capture::ModelEx, api::ApiError> {
-    let media = model::media::ActiveModelEx::from(media1);
+    let media = model::media::ActiveModelEx::from(media_handle).set_user_id(user_id);
 
     let active_model = model::capture::ActiveModel::builder()
         .set_user_id(user_id)
@@ -17,6 +17,8 @@ pub async fn import_capture(
         .add_media(media)
         .save(&db.conn)
         .await?;
+
+    
 
     Ok(active_model.try_into_model()?)
 }
