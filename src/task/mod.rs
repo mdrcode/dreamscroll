a59969fd210c568e629fc1a478c470d844a78bf5
Mike Repass <mike.repass@gmail.com>
2026-03-07 10:28:43 -0500

    one last rename today

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,12 +1,10 @@
-
-mod taskqueue;
-pub use taskqueue::*;
-
 // A handle which converts logical signals (e.g. "new capture 323") into
 // concrete tasks (e.g. "enqueue capture 323 for illumination")
 mod beacon;
 pub use beacon::*;
 
-// Gcloud Pub/Sub implementation of TaskQueue
-pub mod gcloud;
-pub use gcloud::*;
+mod taskqueue;
+pub use taskqueue::*;
+
+pub mod taskqueue_gcloud;
+pub use taskqueue_gcloud::*;
